33da2ec8c3e1201f7d835dda66251d0264bded8b
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2024-10-18 15:42:10 +0000

    Bug 23005: Add language field to the UI

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/Vendors/VendorResource.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/Vendors/VendorResource.vue
@@ -225,6 +225,14 @@ export default {
                     fallbackType: "text",
                 },
                 {
+                    name: "language",
+                    group: $__("Details"),
+                    type: "select",
+                    label: $__("Language"),
+                    avCat: "av_lang",
+                    fallbackType: "text",
+                },
+                {
                     name: "aliases",
                     type: "relationshipWidget",
                     group: $__("Aliases"),
