39b12581dda2774331c731c6c167ecc82ed7d99e
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2024-10-29 17:12:24 +0000

    Bug 38262: Update UI
    (cherry picked from commit 124ef44868ff50fd3c54c56a10495557d470f722)

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/Vendors/VendorResource.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/Vendors/VendorResource.vue
@@ -156,6 +156,7 @@ export default {
             additionalToolbarButtons,
             defaultToolbarButtons,
             stickyToolbar: ["Form"],
+            extendedAttributesResourceType: "vendor",
             resourceAttrs: [
                 {
                     name: "id",
