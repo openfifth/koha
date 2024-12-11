392bba5ba7a8a95ce7b8e0ffb50faaa87946af21
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2024-12-11 10:44:32 +0000

    FINANCES: Add rspack config and apache redirect
    (cherry picked from commit abb0f18003f719ce3b2f43e444407b49fc504c31)
    (cherry picked from commit b6b1860676fdf0eb07f5a76cf4e06c6d32f42ff5)

--- rspack.config.js
+++ rspack.config.js
@@ -28,6 +28,8 @@ module.exports = [
                 "./koha-tmpl/intranet-tmpl/prog/js/vue/modules/acquisitions.ts",
             islands: "./koha-tmpl/intranet-tmpl/prog/js/vue/modules/islands.ts",
             sip2: "./koha-tmpl/intranet-tmpl/prog/js/vue/modules/sip2.ts",
+            "acquisitions/fundManagement":
+                "./koha-tmpl/intranet-tmpl/prog/js/vue/modules/acquisitions/fundManagement.ts",
         },
         output: {
             filename: "[name].js",
