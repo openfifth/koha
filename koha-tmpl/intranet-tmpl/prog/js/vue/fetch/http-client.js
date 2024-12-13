aafb3e32070e18e852b02354ebedc54ed22fb04c
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2024-12-13 16:14:57 +0000

    FINANCES: Enable detailed errors from the API
    (cherry picked from commit 6b6526fdf4156868307eab68cb4df00cb5b84d53)
    (cherry picked from commit 66b0bc9e61c21529343226148e16fff6f3b3b239)

--- koha-tmpl/intranet-tmpl/prog/js/vue/fetch/http-client.js
+++ koha-tmpl/intranet-tmpl/prog/js/vue/fetch/http-client.js
@@ -43,16 +43,21 @@ class HttpClient {
                 if (!response.ok) {
                     return response.text().then(text => {
                         let message;
+                        let errorResponse = {};
                         if (text) {
                             let json = JSON.parse(text);
                             message =
                                 json.error ||
                                 json.errors.map(e => e.message).join("\n") ||
                                 json;
+                            errorResponse = {
+                                message,
+                                ...json,
+                            };
                         } else {
                             message = response.statusText;
                         }
-                        throw new Error(message);
+                        throwDetailedError(message, errorResponse);
                     });
                 }
                 return return_response ? response : response.json();
@@ -61,8 +66,14 @@ class HttpClient {
                 res = result;
             })
             .catch(err => {
-                error = err;
-                setError(err);
+                if (err.dialog_confirm) {
+                    error = err.message;
+                    if (mark_submitting) submitted();
+                    throwDetailedError(err.message, err);
+                } else {
+                    error = err.message;
+                    setError(err.message);
+                }
             })
             .then(() => {
                 if (mark_submitting) submitted();
@@ -198,3 +209,12 @@ class HttpClient {
 }
 
 export default HttpClient;
+
+const throwDetailedError = (message, errorData) => {
+    let errorDetails = new Error(message);
+    errorDetails = {
+        ...errorDetails,
+        ...errorData,
+    };
+    throw errorDetails;
+};
