f6c59bab97e421133846d84579b6b94f812ec5b6
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2025-01-03 15:58:47 +0000

    Add API tests
    (cherry picked from commit 7ad8e1c07e4c1f7f2095250c9fc6b8b76a397970)
    (cherry picked from commit 7f542421d794802602f1cc7bebab6e48493f1d6e)

--- t/lib/TestBuilder.pm
+++ t/lib/TestBuilder.pm
@@ -476,6 +476,7 @@ sub _buildColumnValue {
             return;
         }
     }
+
     return $retvalue;
 }
 
@@ -728,6 +729,12 @@ sub _gen_default_values {
             renewal_priority => undef,
             vendor_id        => undef,
         },
+        FiscalPeriod   => { lib_group_visibility => undef },
+        Ledger         => { lib_group_visibility => undef },
+        Fund           => { lib_group_visibility => undef },
+        SubFund        => { lib_group_visibility => undef },
+        FundAllocation => { lib_group_visibility => undef },
+        FundGroup      => { lib_group_visibility => undef },
     };
 }
 
