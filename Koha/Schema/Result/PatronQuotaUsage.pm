9059e6e69d9bf6b9da393e81359fc592a85541bd
Martin Renvoize <martin.renvoize@ptfs-europe.com>
2025-02-13 09:44:45 +0000

    Bug 38924: Add issue and old_issue relations to Schema
    # Conflicts:
    #	Koha/Schema/Result/Issue.pm
    #	Koha/Schema/Result/PatronQuotaUsage.pm
    
    (cherry picked from commit 8d3ab6ddd081f4ae508ac0bbabc91ac807a1df9d)

---
+++ Koha/Schema/Result/PatronQuotaUsage.pm
@@ -0,0 +1,199 @@
+use utf8;
+package Koha::Schema::Result::PatronQuotaUsage;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Koha::Schema::Result::PatronQuotaUsage
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<patron_quota_usage>
+
+=cut
+
+__PACKAGE__->table("patron_quota_usage");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+unique identifier for quota usage record
+
+=head2 patron_quota_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 0
+
+foreign key linking to patron_quota.id
+
+=head2 issue_id
+
+  data_type: 'integer'
+  is_nullable: 1
+
+linking to issues.issue_id or old_issues.issue_id
+
+=head2 patron_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 0
+
+foreign key linking to borrowers.borrowernumber
+
+=head2 type
+
+  data_type: 'enum'
+  extra: {list => ["ISSUE","RENEW"]}
+  is_nullable: 0
+
+The type of usage this is
+
+=head2 creation_date
+
+  data_type: 'timestamp'
+  datetime_undef_if_invalid: 1
+  default_value: current_timestamp
+  is_nullable: 0
+
+the timestamp for when a usage was recorded
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "patron_quota_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
+  "issue_id",
+  { data_type => "integer", is_nullable => 1 },
+  "patron_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
+  "type",
+  {
+    data_type => "enum",
+    extra => { list => ["ISSUE", "RENEW"] },
+    is_nullable => 0,
+  },
+  "creation_date",
+  {
+    data_type => "timestamp",
+    datetime_undef_if_invalid => 1,
+    default_value => \"current_timestamp",
+    is_nullable => 0,
+  },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+=head1 RELATIONS
+
+=head2 patron
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::Borrower>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "patron",
+  "Koha::Schema::Result::Borrower",
+  { borrowernumber => "patron_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+=head2 patron_quota
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::PatronQuota>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "patron_quota",
+  "Koha::Schema::Result::PatronQuota",
+  { id => "patron_quota_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07051 @ 2025-11-06 17:47:10
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:hxeHkoIaFwVoiLReL04/4A
+
+
+
+=head2 issue
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::Issue>
+
+=cut
+
+__PACKAGE__->belongs_to(
+    issue => 'Koha::Schema::Result::Issue',
+    'issue_id',
+    { join_type => 'left' }
+);
+
+=head2 old_issue
+
+Type: belongs_to
+
+Related object: L<Koha::Schema::Result::OldIssue>
+
+=cut
+
+__PACKAGE__->belongs_to(
+    old_issue => 'Koha::Schema::Result::OldIssue',
+    'issue_id',
+    { join_type => 'left' }
+);
+
+=head2 koha_object_class
+
+Missing POD for koha_object_class.
+
+=cut
+
+sub koha_object_class {
+    'Koha::Patron::Quota::Usage';
+}
+
+=head2 koha_objects_class
+
+Missing POD for koha_objects_class.
+
+=cut
+
+sub koha_objects_class {
+    'Koha::Patron::Quota::Usages';
+}
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
