87aa65b0e3c1f2897ffc2be4227fa180a54dd284
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2025-03-28 11:36:37 +0000

    Bug 38201: Fix for inverse boolean checks
    We currently check for if a boolean is true but have no way of checking for a falsy value, this patch adds a fix for that

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/AgreementResource.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/ERM/AgreementResource.vue
@@ -807,6 +807,7 @@ export default {
                     componentProps: {
                         disabled: {
                             resourceProperty: "by_expired",
+                            qualifier: "!",
                         },
                     },
                     value: "",
