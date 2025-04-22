fb1a470db29d15d87c87dda46af4f653b4a93ae9
Jake Deery <jake.deery@openfifth.co.uk>
2025-04-22 09:45:35 +0100

    Bug xxxxx: Add option to expand responsive datatable
    This patch adds the 'OPACTableColExpandedByDefault' system preference,
    disabled by default, which controls whether responsive datatable rows
    are expanded or not. This work has been performed after recommendations
    from a recent accessibility audit.
    
    TO TEST:
    a)  set 'OpacLocationBranchToDisplay' to both
    b)  search the OPAC for history, select first result
    c)  zoom your browser in to maxiumum, scroll down to holdings table
        -   notice how the rows are collapsed by default, with '+' visible
    **apply patch**
    e)  refresh details page, notice how the behaviour is currently unchanged
    f)  change new 'OPACTableColExpandedByDefault' syspref to 'Expand'
    g)  return to OPAC details page, refresh, zoom in
        -  notice how the rows are now expanded by default, with '-' visible
    **sign off**

--- installer/data/mysql/mandatory/sysprefs.sql
+++ installer/data/mysql/mandatory/sysprefs.sql
@@ -574,6 +574,7 @@ INSERT INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `
 ('OpacSuppression','0','','Turn ON the OPAC Suppression feature, requires further setup, ask your system administrator for details','YesNo'),
 ('OpacSuppressionByIPRange','','','Restrict the suppression to IP adresses outside of the IP range','free'),
 ('OpacSuppressionRedirect','1','Redirect the opac detail page for suppressed records to an explanatory page (otherwise redirect to 404 error page)','','YesNo'),
+('OPACTableColExpandedByDefault','0',NULL,'Determines whether or not table rows are expanded by default on mobile','YesNo'),
 ('opacthemes','bootstrap','','Define the current theme for the OPAC interface.','Themes'),
 ('OpacTopissue','0',NULL,'If ON, enables the \'most popular items\' link on OPAC. Warning, this is an EXPERIMENTAL feature, turning ON may overload your server','YesNo'),
 ('OpacTrustedCheckout', '0', NULL, 'Allow logged in OPAC users to check out to themselves', 'YesNo'),
