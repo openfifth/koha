1412231f59dc410b50b52ad6dea4655e4d8a44df
Chloe Zermatten <chloe.zermatten@openfifth.co.uk>
2025-06-23 15:26:55 +0000

    Bug 39756: feat(old-scripts): script deprecation warnings
    Sponsored-by: Black Hills Library Consortium- http://www.rcgov.org/Library/

--- misc/cronjobs/overdue_notices.pl
+++ misc/cronjobs/overdue_notices.pl
@@ -377,6 +377,14 @@ GetOptions(
 pod2usage(1)               if $help;
 pod2usage( -verbose => 2 ) if $man;
 
+warn "========================================================\n";
+warn "Warning: this script will be deprecated and replaced by\n";
+warn "         process_circulation_triggers.pl. Configuration options\n";
+warn "         for process_circulation_triggers.pl are available at\n";
+warn "         /cgi-bin/koha/admin/circulation_triggers.\n";
+warn "========================================================\n";
+
+
 if ( defined $csvfilename && $csvfilename =~ /^-/ ) {
     warn qq(using "$csvfilename" as filename, that seems odd);
 }
