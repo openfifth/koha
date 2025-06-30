b7106c773eb7fdb452181286f51ba3ff1c8406bf
Jacob O'Mara <jacob.omara@openfifth.co.uk>
2025-06-30 12:22:03 +0100

    Bug 4858: Add staff messaging preferences interface for print notice charging
    - Update messaging-preference-form.inc to include print transport column
    - Update memberentry.pl controller with print charging context
    - Add print charge warning to memberentrygen.tt template
    
    Sponsored-by: OpenFifth <https://openfifth.co.uk\>
    Signed-off-by: Jackie Usher <jackie.usher@westsussex.gov.uk>

--- members/memberentry.pl
+++ members/memberentry.pl
@@ -884,6 +884,16 @@ if ( C4::Context->preference('EnhancedMessagingPreferences') ) {
     $template->param( SMSSendDriver         => C4::Context->preference("SMSSendDriver") );
     $template->param( SMSnumber             => $data{'smsalertnumber'} );
     $template->param( TalkingTechItivaPhone => C4::Context->preference("TalkingTechItivaPhoneNotification") );
+
+    # Print notice charging context - category-based
+    if ($patron && $patron->category && $patron->category->print_notice_charge > 0) {
+        $template->param(
+            print_notice_charging => 1,
+            print_notice_charge_amount => $patron->category->print_notice_charge,
+            patron_has_email => $patron->notice_email_address,
+        );
+    }
+
 }
 
 $template->param( borrower_data    => \%data );
