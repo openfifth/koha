9361bf5c7c39778fa802dd52454612f17ac2a7c1
Jake Deery <jake.deery@openfifth.co.uk>
2025-07-17 09:15:37 +0100

    LOCAL: Replaced package crontab files with empties

--- debian/koha-common.cron.d
+++ debian/koha-common.cron.d
@@ -1,12 +1,6 @@
+#!/bin/sh
 # /etc/cron.d/koha-common
 #
-# Call koha-rebuild-zebra for each enabled Koha instance, to make sure the
-# Zebra indexes are up to date.
-
-SHELL=/bin/sh
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-
-# Uncomment the following line if you do not want to use the koha-index-daemon integration
-# */5 * * * * root test -x /usr/sbin/koha-rebuild-zebra && koha-rebuild-zebra -q $(koha-list --enabled)
-
-*/15 * * * * root koha-foreach --chdir --enabled --email /usr/share/koha/bin/cronjobs/process_message_queue.pl
+# silence is golden
+# see the crontab for the koha-shell user, to modify the crontab on this system
+# use koha-shell -c "crontab -e" user 
