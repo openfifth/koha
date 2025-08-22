3cb99934f97c2cb47bfc37386d623bc18614fa26
Pedro Amorim <pedro.amorim@openfifth.co.uk>
2025-08-22 12:07:10 +0000

    [MELL] Add CSRF hack for Kreablo/plugin-vendor-acquisition
    Fix required for Koha 24.11 documented at:
    https://github.com/Kreablo/koha-plugin-vendor-acquisition/tree/v2.3?tab=readme-ov-file#upgrade

--- Koha/Middleware/CSRF.pm
+++ Koha/Middleware/CSRF.pm
@@ -72,8 +72,11 @@ sub call {
             $error = sprintf "Programming error - op '%s' must start with 'cud-' for %s %s (referer: %s)", $original_op,
                 $request_method, $uri, $referer;
         } elsif ( !$csrf_token ) {
-            $error = sprintf "Programming error - No CSRF token passed for %s %s (referer: %s)", $request_method,
-                $uri, $referer;
+            my $u0 = URI->new($uri);
+            unless ( $u0->path =~ m|/intranet/plugins/run.pl$| && $req->param('method') eq 'vendor_order_receive' ) {
+                $error = sprintf "Programming error - No CSRF token passed for %s %s (referer: %s)", $request_method,
+                    $uri, $referer;
+            }
         } else {
             unless (
                 Koha::Token->new->check_csrf(
