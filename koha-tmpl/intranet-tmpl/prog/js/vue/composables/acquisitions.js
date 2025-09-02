3755251cf893fa0420cb5996608ce326e0c7e8d9
Matt Blenkinsop <matt.blenkinsop@openfifth.co.uk>
2025-09-02 15:21:13 +0100

    ORDERLINES: Move formatValueWithCurrency to the acquisitions composable

--- koha-tmpl/intranet-tmpl/prog/js/vue/composables/acquisitions.js
+++ koha-tmpl/intranet-tmpl/prog/js/vue/composables/acquisitions.js
@@ -6,8 +6,31 @@ const formatFloatingPoint = value => {
     return new BigNumber(value).decimalPlaces(6).toNumber();
 };
 
+const formatValueWithCurrencyHandler = (value, currency, store) => {
+    const formattedPrice = formatFloatingPoint(value).format_price();
+    if (!currency) {
+        return formattedPrice;
+    }
+    const { symbol } = store.currencies.find(
+        curr => curr.currency === currency
+    );
+    if (!value) {
+        return `${symbol}0`;
+    }
+    if (!formattedPrice) {
+        return `${symbol}0`;
+    }
+    if (formattedPrice < 0) {
+        return `-${symbol}${-formattedPrice}`;
+    }
+    return `${symbol}${formattedPrice}`;
+};
+
 export const acquisitionsActions = store => {
     return {
         formatFloatingPoint,
+        formatValueWithCurrency(value, currency) {
+            return formatValueWithCurrencyHandler(value, currency, store);
+        },
     };
 };
