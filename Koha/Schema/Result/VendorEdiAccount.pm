1f5bd33880d9439610974233ba71930521c3bdbb
Jake Deery <jake.deery@openfifth.co.uk>
2025-09-23 15:02:53 +0000

    DBIC - DO NOT PUSH

--- Koha/Schema/Result/VendorEdiAccount.pm
+++ Koha/Schema/Result/VendorEdiAccount.pm
@@ -34,31 +34,16 @@ __PACKAGE__->table("vendor_edi_accounts");
   data_type: 'mediumtext'
   is_nullable: 0
 
-=head2 host
-
-  data_type: 'varchar'
-  is_nullable: 1
-  size: 40
-
-=head2 username
-
-  data_type: 'varchar'
-  is_nullable: 1
-  size: 40
-
-=head2 password
-
-  data_type: 'mediumtext'
-  is_nullable: 1
-
-=head2 upload_port
+=head2 download_sftp_server_id
 
   data_type: 'integer'
+  is_foreign_key: 1
   is_nullable: 1
 
-=head2 download_port
+=head2 upload_sftp_server_id
 
   data_type: 'integer'
+  is_foreign_key: 1
   is_nullable: 1
 
 =head2 last_activity
@@ -102,13 +87,6 @@ __PACKAGE__->table("vendor_edi_accounts");
   default_value: 14
   is_nullable: 1
   size: 3
-
-=head2 transport
-
-  data_type: 'varchar'
-  default_value: 'FTP'
-  is_nullable: 1
-  size: 6
 
 =head2 quotes_enabled
 
@@ -160,16 +138,10 @@ __PACKAGE__->add_columns(
   { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
   "description",
   { data_type => "mediumtext", is_nullable => 0 },
-  "host",
-  { data_type => "varchar", is_nullable => 1, size => 40 },
-  "username",
-  { data_type => "varchar", is_nullable => 1, size => 40 },
-  "password",
-  { data_type => "mediumtext", is_nullable => 1 },
-  "upload_port",
-  { data_type => "integer", is_nullable => 1 },
-  "download_port",
-  { data_type => "integer", is_nullable => 1 },
+  "download_sftp_server_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
+  "upload_sftp_server_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
   "last_activity",
   { data_type => "date", datetime_undef_if_invalid => 1, is_nullable => 1 },
   "vendor_id",
@@ -184,8 +156,6 @@ __PACKAGE__->add_columns(
   { data_type => "varchar", default_value => "EUR", is_nullable => 1, size => 3 },
   "id_code_qualifier",
   { data_type => "varchar", default_value => 14, is_nullable => 1, size => 3 },
-  "transport",
-  { data_type => "varchar", default_value => "FTP", is_nullable => 1, size => 6 },
   "quotes_enabled",
   { data_type => "tinyint", default_value => 0, is_nullable => 0 },
   "invoices_enabled",
@@ -272,8 +242,8 @@ __PACKAGE__->belongs_to(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07049 @ 2024-04-19 16:25:44
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:jenUF3Wx7J7F5270mLQMbw
+# Created by DBIx::Class::Schema::Loader v0.07051 @ 2025-09-23 15:01:48
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:fM9hmuOumA5SmcuhE5AN8w
 
 __PACKAGE__->add_columns(
     '+auto_orders'       => { is_boolean => 1 },
