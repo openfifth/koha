66515ef499d5e6b93db57ca243704c67ae7dd035
Kyle M Hall <kyle@bywatersolutions.com>
2025-10-15 18:06:47 +0000

    Bug 41020: Add delete_file() method to file transport classes
    This patch adds the ability to delete files from remote servers
    through the file transport API. This is needed for the MARC ordering
    feature to clean up processed files from FTP/SFTP servers.
    
    Changes:
    - Added abstract delete_file() method to Koha::File::Transport base class
    - Implemented delete_file() in Koha::File::Transport::FTP using Net::FTP->delete()
    - Implemented delete_file() in Koha::File::Transport::SFTP using Net::SFTP::Foreign->remove()
    - Implemented delete_file() in Koha::File::Transport::Local using unlink()
    - All implementations follow the standard message/error handling pattern
    - Updated polymorphic tests to verify delete_file() capability
    
    The delete_file() method:
    - Takes a filename as parameter (relative to current directory)
    - Returns true on success, undef on failure
    - Adds appropriate success/error messages to the transport message queue
    - Follows the same pattern as existing methods (download_file, rename_file)
    
    Test plan:
    1. prove t/db_dependent/Koha/File/Transports.t
    2. Verify all transport types support delete_file()
    3. Test with MARC ordering cronjob using --dr flag (added in main patch)
    
    Signed-off-by: Martin Renvoize <martin.renvoize@openfifth.co.uk>

--- Koha/File/Transport/FTP.pm
+++ Koha/File/Transport/FTP.pm
@@ -122,7 +122,37 @@ sub _download_file {
     return 1;
 }
 
+=head3 _delete_file
+
+    my $success = $server->_delete_file($filename);
+
+Passed a filename, this will delete the file from the current directory of the server connection.
+
+Returns true on success or undefined on failure.
+
+=cut
+
+sub _delete_file {
+    my ( $self, $remote_file ) = @_;
+    my $operation = "delete";
+
+    $self->{connection}->delete($remote_file) or return $self->_abort_operation( $operation, $remote_file );
+
+    $self->add_message(
+        {
+            message => $operation,
+            type    => 'success',
+            payload => {
+                remote_file => $remote_file,
+            }
+        }
+    );
+
+    return 1;
+}
+
 =head3 _change_directory
+
 
     my $success = $server->_change_directory($directory);
 
