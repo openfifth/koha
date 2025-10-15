4d2033c23d25c9adea91ea7e0d2f21955c854067
Pedro Amorim <pedro.amorim@openfifth.co.uk>
2025-10-15 09:07:25 +0000

    Bug 40504: Preparation: Disambiguation of .clear_search
    This is required, or clicking 'Clear' on the ILL patron search modal will trigger this event listener and cause a JS error, as the 'regular' ILL table is not shown/initialized when editing an ILL request
    
    Signed-off-by: Jeremy Evans <Jeremy.Evans@ukhsa.gov.uk>

--- koha-tmpl/intranet-tmpl/prog/js/ill-list-table.js
+++ koha-tmpl/intranet-tmpl/prog/js/ill-list-table.js
@@ -612,7 +612,7 @@ $(document).ready(function() {
     populateBackendFilter();
 
     // Clear all filters
-    $('.clear_search').click(function() {
+    $("#illfilter_form .clear_search").click(function () {
         clearSearch();
     });
 
