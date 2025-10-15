af01b1195b96a9cefdcdcb0ec8c6368629be6031
Kyle M Hall <kyle@bywatersolutions.com>
2025-10-15 10:03:46 -0400

    Bug 41020: Add ability to use file transports for marc ordering accounts
    FTP and SFTP server management was added to Koha in bug 39190. This feature
    should be made available to the marc order accounts feature so external
    processes are not needed to downloading the marc order files.
    
    Test Plan:
    1) Run updatedatabase.pl
    2) Restart all the things!
    3) Create one or more file transports
    4) Attach those transports to one or more MARC order accounts
    5) Run marc_order_accounts.tt, note the files are transferred from the
       remove server to the "Download directory" of the MARC order account
    6) Add another file to the remote server
    7) Run script with "--rr dl"
    8) Note the remote file has ".dl" append to the filename
    9) Remove the .dl file extension from the remote file
    10) Run script with "--dr"
    11) Note the remote file is deleted
    
    Signed-off-by: Martin Renvoize <martin.renvoize@openfifth.co.uk>
    (cherry picked from commit 4e52319e07e31b1a7dd177232297e39f6730b3d2)

---
+++ installer/data/mysql/atomicupdate/bug_41020.pl
@@ -0,0 +1,28 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_success say_info);
+
+return {
+    bug_number  => "41020",
+    description => "Add file_transport_id to marc_order_accounts",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        if ( !column_exists( 'marc_order_accounts', 'file_transport_id' ) ) {
+            $dbh->do(
+                q{
+                ALTER TABLE `marc_order_accounts`
+                  ADD COLUMN `file_transport_id` int(11) DEFAULT NULL AFTER `basket_name_field`,
+                  ADD KEY `marc_order_accounts_file_transport_id` (`file_transport_id`),
+                  ADD CONSTRAINT `marc_order_accounts_ibfk_file_transport`
+                    FOREIGN KEY (`file_transport_id`)
+                    REFERENCES `file_transports` (`file_transport_id`)
+                    ON DELETE SET NULL
+                    ON UPDATE CASCADE;
+            }
+            );
+        }
+
+        say $out "Added column 'marc_order_accounts.file_transport_id'";
+    },
+};
