ec93908519289ac96c0d2baf9a3e5a7913cb683b
Martin Renvoize <martin.renvoize@openfifth.co.uk>
2026-01-16 08:00:07 +0000

    Bug 41649: Add sip_magnetic field to itemtypes table
    This patch adds the new sip_magnetic boolean flag field to the itemtypes
    table to allow flagging the itemtype as magnetic for sip responses.
    
    (cherry picked from commit c7999ad882c476d1cbe7a97ec4b02db914e228e7)

---
+++ installer/data/mysql/atomicupdate/bug_41649.pl
@@ -0,0 +1,23 @@
+use Modern::Perl;
+use Koha::Installer::Output qw(say_warning say_success say_info);
+
+return {
+    bug_number  => "41649",
+    description => "Add sip_magnetic column to itemtypes table for SIP media handling",
+    up          => sub {
+        my ($args) = @_;
+        my ( $dbh, $out ) = @$args{qw(dbh out)};
+
+        # Add sip_magnetic column to itemtypes table
+        $dbh->do(
+            q{
+            ALTER TABLE itemtypes
+            ADD COLUMN sip_magnetic TINYINT(1) NOT NULL DEFAULT 0
+            COMMENT 'Indicates if items of this type are magnetic media for SIP'
+            AFTER sip_media_type
+        }
+        );
+
+        say_success( $out, "Added column 'itemtypes.sip_magnetic'" );
+    },
+};
