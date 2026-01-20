22755a9a488de0e73413423855c237811572b6e4
Martin Renvoize <martin.renvoize@openfifth.co.uk>
2026-01-20 11:58:44 +0000

    Bug 1649: Make database update idempotent

--- installer/data/mysql/atomicupdate/bug_41649.pl
+++ installer/data/mysql/atomicupdate/bug_41649.pl
@@ -4,20 +4,21 @@ use Koha::Installer::Output qw(say_warning say_success say_info);
 return {
     bug_number  => "41649",
     description => "Add sip_magnetic column to itemtypes table for SIP media handling",
-    up          => sub {
+    up => sub {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
 
         # Add sip_magnetic column to itemtypes table
-        $dbh->do(
-            q{
-            ALTER TABLE itemtypes
-            ADD COLUMN sip_magnetic TINYINT(1) NOT NULL DEFAULT 0
-            COMMENT 'Indicates if items of this type are magnetic media for SIP'
-            AFTER sip_media_type
+        unless ( column_exists( 'itemtypes', 'sip_magnetic' ) ) {
+            $dbh->do(
+                q{
+                ALTER TABLE itemtypes
+                ADD COLUMN sip_magnetic TINYINT(1) NOT NULL DEFAULT 0
+                COMMENT 'Indicates if items of this type are magnetic media for SIP'
+                AFTER sip_media_type
+            }
+            );
+            say_success( $out, "Added column 'itemtypes.sip_magnetic'" );
         }
-        );
-
-        say_success( $out, "Added column 'itemtypes.sip_magnetic'" );
     },
 };
