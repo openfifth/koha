221aea13aaa70aac81c2937e9e525892a1ed6356
Matt Blenkinsop <matt.blenkinsop@openfifth.co.uk>
2026-02-09 14:35:05 +0000

    Allow the Vue PatronSearch modal component to pass additional filters to the modal

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/PatronSearch.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/PatronSearch.vue
@@ -14,6 +14,12 @@
         ><i class="fa fa-plus"></i> {{ $__("Select user") }}</a
     >
     <input
+        type="hidden"
+        name="selected_patron_id"
+        id="additional_patron_filters"
+        :data-additionalfilters="filters"
+    />
+    <input
         v-if="shouldRenderInput"
         type="hidden"
         name="selected_patron_id"
@@ -32,12 +38,30 @@ export default {
         resource: Object,
         label: String,
         required: Boolean,
+        additionalFilters: Object,
+        selectCallback: Function,
     },
     setup(props) {
         const loading = ref(false);
 
         onBeforeMount(() => {
             props.resource.patron_str = $patron_to_html(props.resource.patron);
+        });
+
+        const filters = computed(() => {
+            if (!props.additionalFilters) return "";
+            const checkForRefs = Object.keys(props.additionalFilters).reduce(
+                (acc, filter) => {
+                    if (typeof props.additionalFilters[filter] === "object") {
+                        acc[filter] = props.additionalFilters[filter].value;
+                        return acc;
+                    }
+                    acc[filter] = props.additionalFilters[filter];
+                    return acc;
+                },
+                {}
+            );
+            return JSON.stringify(checkForRefs);
         });
 
         const shouldRenderInput = computed(() => {
@@ -70,6 +94,9 @@ export default {
                 props.resource.patron = patron;
                 props.resource.patron_str = $patron_to_html(patron);
                 props.resource.user_id = patron.patron_id;
+                if (props.selectCallback) {
+                    props.selectCallback(patron);
+                }
                 loading.value = false;
             });
         };
@@ -79,6 +106,7 @@ export default {
             shouldRenderInput,
             selectUser,
             newUserSelected,
+            filters,
         };
     },
 };
