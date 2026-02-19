792c7f81ec56f572c7a339214d19ab0311b97310
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2026-02-19 15:55:42 +0000

    Bug 38924: (follow-up) Record RENEW usage type for renewals
    Pass explicit type param to add_usage: ISSUE for checkouts,
    RENEW for renewals. Default to ISSUE in add_usage for safety.

--- C4/Circulation.pm
+++ C4/Circulation.pm
@@ -1952,6 +1952,7 @@ sub AddIssue {
                         {
                             patron_id => $patron->borrowernumber,
                             issue_id  => $issue->issue_id,
+                            type      => 'ISSUE',
                         }
                     );
                 }
@@ -3390,6 +3391,7 @@ sub AddRenewal {
                     {
                         patron_id => $patron->borrowernumber,
                         issue_id  => $issue->issue_id,
+                        type      => 'RENEW',
                     }
                 );
             }
