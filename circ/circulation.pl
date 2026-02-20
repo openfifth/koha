3f575adc81fe785ea892753f5b79b297f46e0eb5
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2026-02-20 13:36:38 +0000

    Bug 38924: (follow-up) Exclude QUOTA_SELECT from session remember
    Prevents quota selection from being stored in or skipped by the
    session confirmation cookie. Hides the remember checkbox when quota
    is the only confirmation shown.

--- circ/circulation.pl
+++ circ/circulation.pl
@@ -517,6 +517,7 @@ if ( @$barcodes && $op eq 'cud-checkout' ) {
                     next
                         if $patron_match
                         && scalar(@conf_keys) > 0
+                        && $needsconfirmation_key ne 'QUOTA_SELECT'
                         && grep( { $needsconfirmation_key eq $_ } @conf_keys );
 
                     $template_params->{$needsconfirmation_key}    = $needsconfirmation->{$needsconfirmation_key};
@@ -524,7 +525,8 @@ if ( @$barcodes && $op eq 'cud-checkout' ) {
                     $template_params->{getBarcodeMessageIteminfo} = $item->barcode;
                     $template_params->{NEEDSCONFIRMATION}         = 1;
                     $confirm_required                             = 1;
-                    push( @{ $template_params->{sessionConfirmationKeys} }, $needsconfirmation_key );
+                    push( @{ $template_params->{sessionConfirmationKeys} }, $needsconfirmation_key )
+                        unless $needsconfirmation_key eq 'QUOTA_SELECT';
                 }
             }
             unless ($confirm_required) {
