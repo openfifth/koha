92f1fffca6797b801b5b05812515daef7324036e
Martin Renvoize <martin.renvoize@openfifth.co.uk>
2026-02-25 11:58:35 +0000

    Bug 10190: fix four more issues spotted in the overdues script
    - Patron.pm has_restricting_overdues: sort ASC not DESC so the oldest
      (most overdue) item is checked per branch/itemtype; DESC caused the
      deduplication skip to discard the very items most likely to trigger
      a restriction
    - overdue_notices.pl: fix --itemtypesout option name in GetOptions
      (was 'itemtypeouts', never matched the documented --itemtypesout flag)
    - overdue_notices.pl: fix typo 'catagorycode' -> 'categorycode' in
      --borcatout search, which silently broke category exclusion
    - circulation_triggers.pl: add flagsrequired => parameters/manage_circ_rules
      so the admin page enforces the correct permission; also add missing
      use Koha::Notice::Templates
    
    Signed-off-by: Michaela <michaela.sieber@kit.edu>

--- misc/cronjobs/overdue_notices.pl
+++ misc/cronjobs/overdue_notices.pl
@@ -364,7 +364,7 @@ GetOptions(
     'text:s'         => \$text_filename,    # this optional argument gets '' if not supplied.
     'itemscontent=s' => \$itemscontent,
     'itemtypes=s'    => \@myitemtypes,
-    'itemtypeouts=s' => \@myitemtypesout,
+    'itemtypesout=s' => \@myitemtypesout,
     'list-all'       => \$listall,
     't|triggered'    => \$triggered,
     'test'           => \$test_mode,
@@ -497,7 +497,7 @@ my @categories;
 if (@myborcat) {
     @categories = @myborcat;
 } elsif (@myborcatout) {
-    @categories = Koha::Patron::Categories->search( { catagorycode => { 'not_in' => \@myborcatout } } )
+    @categories = Koha::Patron::Categories->search( { categorycode => { 'not_in' => \@myborcatout } } )
         ->get_column('categorycode');
 } else {
     @categories = Koha::Patron::Categories->search()->get_column('categorycode');
