7188e8f4ce61830c9228209e0a3d296279db6212
Martin Renvoize <martin.renvoize@openfifth.co.uk>
2026-02-26 06:53:35 +0000

    Bug 33886: Replace DateInputStyle syspref with data-flatpickr-split attribute
    Remove the DateInputStyle system preference and instead control split
    date input mode via a data-flatpickr-split="true" attribute on individual
    fields. This allows per-field control rather than a global setting.
    
    Apply split inputs to fields where they are most appropriate:
    - dateofbirth (memorable date, typically far in the past)
    - dateexpiry (far-future date, analogous to card expiry)
    
    The atomicupdate is updated to delete the preference on upgrade.

--- installer/data/mysql/atomicupdate/bug_33886.pl
+++ installer/data/mysql/atomicupdate/bug_33886.pl
@@ -2,21 +2,14 @@ use Modern::Perl;
 
 return {
     bug_number  => "33886",
-    description => "Add DateInputStyle system preference",
+    description => "Remove DateInputStyle system preference",
     up          => sub {
         my ($args) = @_;
         my ( $dbh, $out ) = @$args{qw(dbh out)};
 
         $dbh->do(
             q{
-                INSERT IGNORE INTO systempreferences ( variable, value, options, explanation, type )
-                VALUES (
-                    'DateInputStyle',
-                    'single',
-                    'single|split',
-                    'Controls how date input fields are displayed. "single" uses the standard flatpickr calendar widget with maskito input masking. "split" shows three separate inputs for day, month, and year (order follows the dateformat preference) with an optional calendar button; does not apply to time, datetime, or booking date range fields.',
-                    'Choice'
-                )
+                DELETE FROM systempreferences WHERE variable = 'DateInputStyle'
             }
         );
 
