1cc5557138330597aed233114312b3c6926c031c
Lucas Gass <lucas@bywatersolutions.com>
2026-02-27 18:29:13 +0000

    Bug 36506: DBIC
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    (cherry picked from commit 0a58a77d1d01e7213c9f9408ebcf32a238455a31)

--- Koha/Schema/Result/Itemtype.pm
+++ Koha/Schema/Result/Itemtype.pm
@@ -96,14 +96,6 @@ controls if the hourly rental fee is calculated directly or using finesCalendar
 
 default replacement cost
 
-=head2 processfee
-
-  data_type: 'decimal'
-  is_nullable: 1
-  size: [28,6]
-
-default text be recorded in the column note when the processing fee is applied
-
 =head2 notforloan
 
   data_type: 'tinyint'
@@ -221,8 +213,6 @@ __PACKAGE__->add_columns(
   "rentalcharge_hourly_calendar",
   { data_type => "tinyint", default_value => 1, is_nullable => 0 },
   "defaultreplacecost",
-  { data_type => "decimal", is_nullable => 1, size => [28, 6] },
-  "processfee",
   { data_type => "decimal", is_nullable => 1, size => [28, 6] },
   "notforloan",
   { data_type => "tinyint", default_value => 0, is_nullable => 0 },
