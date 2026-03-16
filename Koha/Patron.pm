2d8ea77e61a1376553f80d5b9a6a9f579afbb50b
Martin Renvoize <martin.renvoize@openfifth.co.uk>
2026-03-16 14:55:04 +0000

    Bug 42085: Slim down members/member-flags.pl cud-newflags block
    Replace the raw-DBI permission write block (~62 lines) with a call to
    $patron->set_permissions(\%granted). Logging and the superlibrarian
    guard are now handled inside the object method.
    
    The CGI layer now only parses the flag params into a granted hashref
    and delegates — keeping the controller thin and the logic testable.
    
    Signed-off-by: David Nind <david@davidnind.com>
    Signed-off-by: Andrew Fuerste Henry <andrew@bywatersolutions.com>
    Signed-off-by: Lucas Gass <lucas@bywatersolutions.com>
    (cherry picked from commit 757526cbde5a51d5cf20c3d454669869db30e7a9)

--- Koha/Patron.pm
+++ Koha/Patron.pm
@@ -3981,7 +3981,7 @@ sub set_permissions {
     );
 
     if ( C4::Context->preference('BorrowersLog') ) {
-        my $after   = $self->get_from_storage->permissions();
+        my $after   = $self->permissions();
         my $changed = diff( $before, $after, noU => 1 );
         if ( keys %$changed ) {
             logaction(
