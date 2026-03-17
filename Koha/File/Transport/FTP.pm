05c3adccaa94ce38172be9830bc616aa578020ef
Martin Renvoize <martin.renvoize@openfifth.co.uk>
2026-03-17 07:51:32 +0000

    Bug 41020: (QA follow-up) Fix FTP crash and guard remote ops with --confirm
    _abort_operation() crashed when both $path and $self->{connection} were
    undef (e.g. on a failed Net::FTP->new call), because the fallback
    expression called ->pwd on undef.
    
    Remote file transport operations (connect, download, delete/rename on
    remote) were not guarded by the --confirm flag, causing them to run in
    dry-run mode. All transport operations now require --confirm. Error/
    failure messages from the transport block now also respect --verbose.

--- Koha/File/Transport/FTP.pm
+++ Koha/File/Transport/FTP.pm
@@ -304,7 +304,7 @@ sub _abort_operation {
             payload => {
                 detail => $self->{connection} ? $self->{connection}->status  : '',
                 error  => $self->{connection} ? $self->{connection}->message : $@,
-                path   => $path               ? $path                        : $self->{connection}->pwd,
+                path   => $path // ( $self->{connection} ? $self->{connection}->pwd : '' ),
             }
         }
     );
