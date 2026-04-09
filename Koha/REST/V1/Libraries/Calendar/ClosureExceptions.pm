dae084a8e5aaf7a7593c8993cf41e3fefe2eba8e
Tomás Cohen Arazi <tomascohen@theke.io>
2026-04-09 10:21:28 -0300

    Bug 42311: REST API CRUD endpoints for library calendar
    With the Koha::Calendar::* classes and the normalized schema from bug 42310
    in place, this patch adds the REST API layer for managing library calendar
    closures.
    
    Each closure type gets its own controller and spec file, following the
    established Koha::REST::V1::Cities pattern:
    
    GET    /libraries/{id}/calendar
      Returns all closure definitions grouped by type.
    
    POST   /libraries/{id}/calendar/weekly_closures
    GET    /libraries/{id}/calendar/weekly_closures/{weekly_closure_id}
    PUT    /libraries/{id}/calendar/weekly_closures/{weekly_closure_id}
    DELETE /libraries/{id}/calendar/weekly_closures/{weekly_closure_id}
    
    POST   /libraries/{id}/calendar/repeating_closures
    GET    /libraries/{id}/calendar/repeating_closures/{repeating_closure_id}
    PUT    /libraries/{id}/calendar/repeating_closures/{repeating_closure_id}
    DELETE /libraries/{id}/calendar/repeating_closures/{repeating_closure_id}
    
    POST   /libraries/{id}/calendar/single_closures
    GET    /libraries/{id}/calendar/single_closures/{single_closure_id}
    PUT    /libraries/{id}/calendar/single_closures/{single_closure_id}
    DELETE /libraries/{id}/calendar/single_closures/{single_closure_id}
    
    POST   /libraries/{id}/calendar/closure_exceptions
    GET    /libraries/{id}/calendar/closure_exceptions/{closure_exception_id}
    PUT    /libraries/{id}/calendar/closure_exceptions/{closure_exception_id}
    DELETE /libraries/{id}/calendar/closure_exceptions/{closure_exception_id}
    
    POST   /libraries/{id}/calendar/copy
      Copies all closure rules from another library.
    
    Permissions: catalogue:1 for reads, tools:edit_calendar for writes.
    
    To test:
    1. Apply the patches from bug 42310, then this one
    2. Run:
       $ ktd --shell
       k$ yarn api:bundle
       k$ prove t/db_dependent/api/v1/libraries_calendar.t
       => SUCCESS: Tests pass!
    3. Sign off :-D
    
    Signed-off-by: Tomás Cohen Arazi <tomascohen@theke.io>
    Signed-off-by: David Nind <david@davidnind.com>
    Signed-off-by: Martin Renvoize <martin.renvoize@openfifth.co.uk>

---
+++ Koha/REST/V1/Libraries/Calendar/ClosureExceptions.pm
@@ -0,0 +1,134 @@
+package Koha::REST::V1::Libraries::Calendar::ClosureExceptions;
+
+# Copyright 2026 Theke Solutions
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <https://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use Mojo::Base 'Mojolicious::Controller';
+
+use Koha::Libraries;
+use Koha::Calendar::Exceptions;
+
+use Try::Tiny qw( catch try );
+
+=head1 API
+
+=head2 Methods
+
+=head3 add
+
+Controller function that handles adding a new closure exception.
+
+=cut
+
+sub add {
+    my $c = shift->openapi->valid_input or return;
+
+    my $library = Koha::Libraries->find( $c->param('library_id') );
+    return $c->render_resource_not_found("Library") unless $library;
+
+    return try {
+        my $closure = $library->calendar->add_exception( $c->req->json );
+        $c->res->headers->location( $c->req->url->to_string . '/' . $closure->id );
+        return $c->render( status => 201, openapi => $c->objects->to_api($closure) );
+    } catch {
+        if ( blessed $_ and $_->isa('Koha::Exceptions::Object::DuplicateID') ) {
+            return $c->render(
+                status  => 409,
+                openapi => { error => 'Closure already exists', error_code => 'duplicate' }
+            );
+        }
+        $c->unhandled_exception($_);
+    };
+}
+
+=head3 get
+
+Controller function that handles retrieving a single closure exception.
+
+=cut
+
+sub get {
+    my $c = shift->openapi->valid_input or return;
+
+    my $library = Koha::Libraries->find( $c->param('library_id') );
+    return $c->render_resource_not_found("Library") unless $library;
+
+    return try {
+        my $closure = Koha::Calendar::Exceptions->find( $c->param('closure_exception_id') );
+        return $c->render_resource_not_found("Closure exception")
+            unless $closure && $closure->library_id eq $library->branchcode;
+        return $c->render( status => 200, openapi => $c->objects->to_api($closure) );
+    } catch {
+        $c->unhandled_exception($_);
+    };
+}
+
+=head3 update
+
+Controller function that handles updating a closure exception.
+
+=cut
+
+sub update {
+    my $c = shift->openapi->valid_input or return;
+
+    my $library = Koha::Libraries->find( $c->param('library_id') );
+    return $c->render_resource_not_found("Library") unless $library;
+
+    my $closure = Koha::Calendar::Exceptions->find( $c->param('closure_exception_id') );
+    return $c->render_resource_not_found("Closure exception")
+        unless $closure && $closure->library_id eq $library->branchcode;
+
+    return try {
+        $closure->set_from_api( $c->req->json );
+        $closure->store;
+        return $c->render( status => 200, openapi => $c->objects->to_api($closure) );
+    } catch {
+        $c->unhandled_exception($_);
+    };
+}
+
+=head3 delete
+
+Controller function that handles deleting a closure exception.
+
+=cut
+
+sub delete {
+    my $c = shift->openapi->valid_input or return;
+
+    my $library = Koha::Libraries->find( $c->param('library_id') );
+
+    return $c->render_resource_not_found("Library")
+        unless $library;
+
+    my $closure = Koha::Calendar::Exceptions->find( $c->param('closure_exception_id') );
+
+    return $c->render_resource_not_found("Closure exception")
+        unless $closure && $closure->library_id eq $library->branchcode;
+
+    return try {
+        $closure->delete;
+        return $c->render_resource_deleted;
+    } catch {
+        $c->unhandled_exception($_);
+    };
+}
+
+1;
