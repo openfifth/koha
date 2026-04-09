0847e4e5a37c916df45742c20022009af041a664
Matt Blenkinsop <matt.blenkinsop@openfifth.co.uk>
2026-04-09 17:54:44 +0100

    FRAMEWORK: Add an InputButton for rendering buttons in forms

---
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/InputButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <input
+        @click="callback && callback()"
+        :class="cssClass"
+        type="button"
+        :value="title"
+    />
+</template>
+
+<script>
+import { $__ } from "@koha-vue/i18n";
+export default {
+    name: "ButtonSubmit",
+    props: {
+        title: {
+            type: String,
+            default: $__("Submit"),
+            required: false,
+        },
+        icon: {
+            type: String,
+            required: false,
+        },
+        cssClass: {
+            type: String,
+            default: "btn btn-primary",
+            required: false,
+        },
+        callback: {
+            type: Function,
+        },
+    },
+};
+</script>
