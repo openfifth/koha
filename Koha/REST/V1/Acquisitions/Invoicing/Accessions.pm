e23b4c089a8fb1f7a96669fc2bde6dd4cd52fc11
Matt Blenkinsop <matt.blenkinsop@openfifth.co.uk>
2026-05-06 14:48:33 +0100

    INVOICING: Add API endpoints and controllers

---
+++ Koha/REST/V1/Acquisitions/Invoicing/Accessions.pm
@@ -0,0 +1,138 @@
+package Koha::REST::V1::Acquisitions::Invoicing::Accessions;
+
+# Copyright 2024 PTFS Europe
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use Mojo::Base 'Mojolicious::Controller';
+use Try::Tiny;
+
+use Koha::Acquisition::Invoicing::Accession;
+use Koha::Acquisition::Invoicing::Accessions;
+
+=head1 API
+
+=head2 Methods
+
+=head3 list
+
+=cut
+
+sub list {
+    my $c = shift->openapi->valid_input or return;
+
+    return try {
+        my $accessions = $c->objects->search( Koha::Acquisition::Invoicing::Accessions->new );
+        return $c->render( status => 200, openapi => $accessions );
+    } catch {
+        $c->unhandled_exception($_);
+    };
+}
+
+=head3 get
+
+=cut
+
+sub get {
+    my $c = shift->openapi->valid_input or return;
+
+    return try {
+        my $accession = Koha::Acquisition::Invoicing::Accessions->find( $c->param('accession_id') );
+        return $c->render_resource_not_found("Accession")
+            unless $accession;
+
+        return $c->render( status => 200, openapi => $c->objects->to_api($accession) );
+    } catch {
+        $c->unhandled_exception($_);
+    };
+}
+
+=head3 add
+
+=cut
+
+sub add {
+    my $c = shift->openapi->valid_input or return;
+
+    return try {
+        Koha::Database->new->schema->txn_do(
+            sub {
+                my $body      = $c->req->json;
+                my $accession = Koha::Acquisition::Invoicing::Accession->new_from_api($body)->store->discard_changes;
+
+                $c->res->headers->location( $c->req->url->to_string . '/' . $accession->accession_id );
+                return $c->render(
+                    status  => 201,
+                    openapi => $c->objects->to_api($accession)
+                );
+            }
+        );
+    } catch {
+        return $c->unhandled_exception($_);
+    };
+}
+
+=head3 update
+
+=cut
+
+sub update {
+    my $c = shift->openapi->valid_input or return;
+
+    my $accession = Koha::Acquisition::Invoicing::Accessions->find( $c->param('accession_id') );
+    return $c->render_resource_not_found("Accession")
+        unless $accession;
+
+    return try {
+        Koha::Database->new->schema->txn_do(
+            sub {
+                my $body = $c->req->json;
+                $accession->set_from_api($body)->store;
+
+                $c->res->headers->location( $c->req->url->to_string . '/' . $accession->accession_id );
+                return $c->render(
+                    status  => 200,
+                    openapi => $c->objects->to_api($accession)
+                );
+            }
+        );
+    } catch {
+        $c->unhandled_exception($_);
+    };
+}
+
+=head3 delete
+
+=cut
+
+sub delete {
+    my $c = shift->openapi->valid_input or return;
+
+    my $accession = Koha::Acquisition::Invoicing::Accessions->find( $c->param('accession_id') );
+    return $c->render_resource_not_found("Accession")
+        unless $accession;
+
+    return try {
+        $accession->delete;
+        return $c->render_resource_deleted;
+    } catch {
+        $c->unhandled_exception($_);
+    };
+}
+
+1;
