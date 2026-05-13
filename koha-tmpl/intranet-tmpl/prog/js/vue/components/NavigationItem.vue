7093aa5eabbf34591b7373d126ce641be18ccfa5
Matt Blenkinsop <matt.blenkinsop@openfifth.co.uk>
2026-05-13 15:38:41 +0100

    FRAMEWORK: Allow left menu links to be collapsible

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/NavigationItem.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/NavigationItem.vue
@@ -29,31 +29,70 @@
                 v-else
                 href="#"
                 aria-current="page"
-                :class="{ disabled: item.disabled }"
+                :class="{ disabled: item.disabled, collapsible: isCollapsible }"
+                @click.prevent="toggleChildren"
             >
                 <template v-if="item.icon">
                     <i :class="`${item.icon}`"></i>&nbsp;
                 </template>
                 <span class="" v-if="item.title">{{ $__(item.title) }}</span>
+                <i
+                    v-if="isCollapsible"
+                    :class="
+                        isExpanded ? 'fa fa-caret-down' : 'fa fa-caret-right'
+                    "
+                    class="nav-collapse"
+                ></i>
             </a>
         </span>
-        <ul v-if="item.children && item.children.length">
-            <NavigationItem
-                v-for="(item, key) in item.children"
-                :item="item"
-            ></NavigationItem>
-        </ul>
+        <template v-if="hasChildren">
+            <ul v-show="isExpanded">
+                <NavigationItem
+                    v-for="(item, key) in item.children"
+                    :key="key"
+                    :item="item"
+                ></NavigationItem>
+            </ul>
+        </template>
     </li>
 </template>
 
 <script>
+import { ref, computed } from "vue";
+
 export default {
     name: "NavigationItem",
     props: {
         item: Object,
         params: Object,
     },
+    setup(props) {
+        const isExpanded = ref(!props.item.collapsible);
+
+        const hasChildren = computed(
+            () => !!(props.item.children && props.item.children.length)
+        );
+
+        const isCollapsible = computed(
+            () => hasChildren.value && !!props.item.collapsible
+        );
+
+        const toggleChildren = () => {
+            if (isCollapsible.value) isExpanded.value = !isExpanded.value;
+        };
+
+        return { isExpanded, hasChildren, isCollapsible, toggleChildren };
+    },
 };
 </script>
 
-<style></style>
+<style scoped>
+.nav-collapse {
+    margin-left: 0.35em;
+    font-size: 0.85em;
+}
+a.disabled.collapsible {
+    pointer-events: auto;
+    cursor: pointer;
+}
+</style>
