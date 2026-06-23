c7b5a363e9a9e774df9278a37a22fb5c9e422a33
Martin Renvoize <martin.renvoize@openfifth.co.uk>
2026-06-23 12:59:04 +0100

    Bug 40136: (RM follow-up) Add missing pod coverage for private subs

--- Koha/Biblio.pm
+++ Koha/Biblio.pm
@@ -2402,7 +2402,30 @@ sub merge_with {
 
 =head2 Internal methods
 
-=head3 type
+=head3 _unblessed_for_log
+
+Returns a plain hashref of biblio column values safe for JSON serialisation
+and action log diffing. Timestamp is excluded as it changes on every write,
+and undef/empty-string fields are removed so that genuinely unset columns
+do not produce noise in diffs.
+
+=cut
+
+sub _unblessed_for_log {
+    my ($self) = @_;
+    my $data = $self->unblessed;
+    delete $data->{timestamp};
+    for my $key ( keys %$data ) {
+        if ( defined $data->{$key} && $data->{$key} ne '' ) {
+            $data->{$key} = "$data->{$key}" if blessed( $data->{$key} );
+        } else {
+            delete $data->{$key};
+        }
+    }
+    return $data;
+}
+
+=head3 _type
 
 =cut
 
