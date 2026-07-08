0456c54b8b5a256660cfd2fae3afc128be77c3df
Olivia Reynolds <olivia.reynolds@openfifth.co.uk>
2026-07-08 14:23:07 +0000

    Bug 30557: Add item list object classes

---
+++ Koha/Item/ListContent.pm
@@ -0,0 +1,41 @@
+package Koha::Item::ListContent;
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <https://www.gnu.org/licenses>.
+
+use Modern::Perl;
+
+use Koha::Database;
+use Koha::Patrons;
+
+use base qw(Koha::Object);
+
+=head1 NAME
+
+Koha::Item::ListContent - Koha Item List content object class
+
+=head2 Internal methods
+
+=head3 _type
+
+The Result type of this object.
+
+=cut
+
+sub _type {
+    return 'ItemListContent';
+}
+
+1;
