db62e5a06dd7cb50286a719714bf9390445977a1
Olivia Reynolds <olivia.reynolds@openfifth.co.uk>
2026-07-20 09:28:09 +0000

    Bug 43141: DB updates
    Adds a table for storing prompts to patrons for filling out type
    disclaimers. As these disclaimers will be sent via notices, a new
    messaging preference is also introduced and enabled by default.
    
    Sponsored-by: NHS England

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -3702,6 +3702,28 @@ CREATE TABLE `identity_providers` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `ill_type_disclaimer_prompts`
+--
+
+DROP TABLE IF EXISTS `ill_type_disclaimer_prompts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ill_type_disclaimer_prompts` (
+  `uuid` varchar(128) NOT NULL COMMENT 'a unique token for this prompt',
+  `patron_id` int(11) NOT NULL COMMENT 'Patron this disclaimer prompt is for',
+  `illrequest_id` bigint(20) unsigned NOT NULL COMMENT 'ILL request this disclaimer prompt is for',
+  `date_prompted` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'when this prompt was created',
+  `date_replied` timestamp NULL DEFAULT NULL COMMENT 'if non-null, when this prompt was replied to',
+  `valid_until` timestamp NOT NULL COMMENT 'when this prompt expires',
+  PRIMARY KEY (`uuid`),
+  UNIQUE KEY `ill_type_disclaimer_uniq` (`patron_id`,`illrequest_id`),
+  KEY `ill_type_disclaimer_prompts_fk_illrequest_id` (`illrequest_id`),
+  CONSTRAINT `ill_type_disclaimer_prompts_fk_illrequest_id` FOREIGN KEY (`illrequest_id`) REFERENCES `illrequests` (`illrequest_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `ill_type_disclaimer_prompts_fk_patron_id` FOREIGN KEY (`patron_id`) REFERENCES `borrowers` (`borrowernumber`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `illbatch_statuses`
 --
 
