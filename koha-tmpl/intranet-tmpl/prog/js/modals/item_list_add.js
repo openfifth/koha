ee7c851b2ea28bf370e521f8a1267cdabf1ad939
Olivia Reynolds <olivia.reynolds@openfifth.co.uk>
2026-09-02 13:13:48 +0000

    Bug 30557: (follow-up) Add "add and view" to item list modal

--- koha-tmpl/intranet-tmpl/prog/js/modals/item_list_add.js
+++ koha-tmpl/intranet-tmpl/prog/js/modals/item_list_add.js
@@ -1,12 +1,18 @@
 function showItemListAddModal() {
     $("#modal-item-list-add").modal("show");
     $("#modal-item-list-add-submit").removeAttr("disabled");
+    $("#modal-item-list-add-submit-view").removeAttr("disabled");
 }
 
 $(document).ready(function () {
     $("#modal-item-list-add-form").validate({
-        submitHandler: function (form) {
+        submitHandler: function (form, e) {
             $("#modal-item-list-add-submit").attr("disabled", "disabled");
+            $("#modal-item-list-add-submit-view").attr("disabled", "disabled");
+
+            const and_view =
+                e.originalEvent.submitter.id ===
+                "modal-item-list-add-submit-view";
 
             let itemnumbers = new Array();
             $("input[name='itemnumber'][type='checkbox']:checked").each(
@@ -43,6 +49,10 @@ $(document).ready(function () {
             $.ajax(settings)
                 .done(function (response) {
                     $("#modal-item-list-add").modal("hide");
+                    if (and_view) {
+                        window.location.href =
+                            "/cgi-bin/koha/lists/items/" + item_list_id;
+                    }
                 })
                 .fail(function (err) {
                     var message =
