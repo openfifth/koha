2faaa4f01da862b63e3f16416fdc0259202dc490
Olivia Reynolds <olivia.reynolds@openfifth.co.uk>
2026-09-02 13:36:07 +0000

    Bug 30557: (follow-up) Allow creating a new list from modal

--- koha-tmpl/intranet-tmpl/prog/js/modals/item_list_add.js
+++ koha-tmpl/intranet-tmpl/prog/js/modals/item_list_add.js
@@ -5,6 +5,14 @@ function showItemListAddModal() {
 }
 
 $(document).ready(function () {
+    $("#item-list-add-form-select").change(function (e) {
+        const show_create = e.target.value === "";
+        if (show_create) {
+            $("#item-list-add-form-new-list").show();
+        } else {
+            $("#item-list-add-form-new-list").hide();
+        }
+    });
     $("#modal-item-list-add-form").validate({
         submitHandler: function (form, e) {
             $("#modal-item-list-add-submit").attr("disabled", "disabled");
@@ -13,6 +21,38 @@ $(document).ready(function () {
             const and_view =
                 e.originalEvent.submitter.id ===
                 "modal-item-list-add-submit-view";
+
+            function add_to_list(item_list_id, itemnumbers) {
+                const url = `/api/v1/item-lists/${item_list_id}/items`;
+
+                var settings = {
+                    url: url,
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    data: JSON.stringify({
+                        item_ids: itemnumbers,
+                    }),
+                };
+
+                $.ajax(settings)
+                    .done(function (response) {
+                        $("#modal-item-list-add").modal("hide");
+                        if (and_view) {
+                            window.location.href =
+                                "/cgi-bin/koha/lists/items/" + item_list_id;
+                        }
+                    })
+                    .fail(function (err) {
+                        var message =
+                            err.responseJSON.error ||
+                            err.responseJSON.errors
+                                .map(e => e.message)
+                                .join("\n");
+                        alert(message);
+                    });
+            }
 
             let itemnumbers = new Array();
             $("input[name='itemnumber'][type='checkbox']:checked").each(
@@ -33,33 +73,43 @@ $(document).ready(function () {
             }
 
             const item_list_id = $("#item-list-add-form-select").val();
-            const url = `/api/v1/item-lists/${item_list_id}/items`;
 
-            var settings = {
-                url: url,
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                data: JSON.stringify({
-                    item_ids: itemnumbers,
-                }),
-            };
+            if (item_list_id === "") {
+                // Create a new list
+                var settings = {
+                    url: "/api/v1/item-lists",
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    data: JSON.stringify({
+                        name: $("#item-list-add-form-new-list-name").val(),
+                        visibility: "private",
+                        owner: $("#item-list-add-form-new-list-owner").val(),
+                    }),
+                };
 
-            $.ajax(settings)
-                .done(function (response) {
-                    $("#modal-item-list-add").modal("hide");
-                    if (and_view) {
-                        window.location.href =
-                            "/cgi-bin/koha/lists/items/" + item_list_id;
-                    }
-                })
-                .fail(function (err) {
-                    var message =
-                        err.responseJSON.error ||
-                        err.responseJSON.errors.map(e => e.message).join("\n");
-                    alert(message);
-                });
+                $.ajax(settings)
+                    .done(function (response) {
+                        $("#item-list-add-form-select").append(
+                            $("<option>", {
+                                value: response.id,
+                                text: response.name,
+                            })
+                        );
+                        add_to_list(response.id, itemnumbers);
+                    })
+                    .fail(function (err) {
+                        var message =
+                            err.responseJSON.error ||
+                            err.responseJSON.errors
+                                .map(e => e.message)
+                                .join("\n");
+                        alert(message);
+                    });
+            } else {
+                add_to_list(item_list_id, itemnumbers);
+            }
         },
     });
 });
